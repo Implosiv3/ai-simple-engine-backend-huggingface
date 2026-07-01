f1c9ab7edf46a340369b955df67110ea92e02183
danialcala94 <danielalcalavalera@gmail.com>
2026-07-01 20:53:47 +0700

    Added 'ai-simple-engine-common' dependency. The 'consts' module has been refactored. Code refactored. Poetry lock file updated. New version.

--- src/ai_simple_engine_backend_huggingface/consts.py
+++ src/ai_simple_engine_backend_huggingface/consts.py
@@ -1,5 +1,6 @@
-HUGGINGFACE_BACKEND_PROVIDER_NAME = 'huggingface'
-"""
-The name of the `provider` property for the
-`HuggingfaceBackend` class.
-"""
+from ai_simple_engine_common.consts.model_spec.providers import HUGGINGFACE_MODEL_BACKEND_PROVIDER
+
+
+__all__ = [
+    'HUGGINGFACE_MODEL_BACKEND_PROVIDER'
+]
